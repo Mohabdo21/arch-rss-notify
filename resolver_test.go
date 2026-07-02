fca8679ca2e6328187a27742568e88df2c41d07f
Mohabdo21 <mohannadabdo21@hotmail.com>
2026-07-02 13:45:05 +0400

    test: add failing tests for version resolvers

---
+++ resolver_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/mmcdole/gofeed"
+)
+
+func TestStandardResolver_Resolve(t *testing.T) {
+	resolver := &StandardResolver{}
+	item := &gofeed.Item{Title: "linux 6.1.0-1"}
+	pkg, ver, err := resolver.Resolve(context.Background(), item)
+	if err != nil || pkg != "linux" || ver != "6.1.0-1" {
+		t.Errorf("expected linux 6.1.0-1, got %s %s (err: %v)", pkg, ver, err)
+	}
+}
+
+func TestAURResolver_Resolve(t *testing.T) {
+	// Mock server for AUR RPC
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `{"results": [{"Version": "7.1.0-1"}]}`)
+	}))
+	defer ts.Close()
+
+	resolver := &AURResolver{BaseURL: ts.URL}
+	item := &gofeed.Item{Title: "zoom"}
+	pkg, ver, err := resolver.Resolve(context.Background(), item)
+	if err != nil || pkg != "zoom" || ver != "7.1.0-1" {
+		t.Errorf("expected zoom 7.1.0-1, got %s %s (err: %v)", pkg, ver, err)
+	}
+}
